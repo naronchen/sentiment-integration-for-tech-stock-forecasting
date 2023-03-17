8a9cd5b59eb5ebff50139156adfff6da88fdf848
Naron <naron.chen@richmond.edu>
2023-03-17 16:16:52 -0400

    wrote this as a class

--- request.py
+++ request.py
@@ -1,46 +1,36 @@
 from dotenv import load_dotenv
 import os
 import requests
-import json
 
+class TwitterAPI:
+    def __init__(self, usernames_list):
+        # Load environment variables from .env file
+        load_dotenv()
+        # Get the bearer token from the environment variable
+        self.bearer_token = os.getenv('BEARER_TOKEN')
+        self.usernames_list = usernames_list
+        self.user_fields = "user.fields=public_metrics"
 
+    def create_url(self):
+        # Join usernames with commas
+        usernames = ','.join(self.usernames_list)
+        url = f"https://api.twitter.com/2/users/by?usernames={usernames}&{self.user_fields}"
+        return url
 
-# Load environment variables from .env file
-load_dotenv()
+    def bearer_oauth(self, r):
+        r.headers["Authorization"] = f"Bearer {self.bearer_token}"
+        r.headers["User-Agent"] = "v2UserLookupPython"
+        return r
 
-# Get the bearer token from the environment variable
-bearer_token = os.getenv('BEARER_TOKEN')
+    def connect_to_endpoint(self, url):
+        response = requests.request("GET", url, auth=self.bearer_oauth)
+        if response.status_code != 200:
+            raise Exception(f"Request returned an error: {response.status_code} {response.text}")
+        return response.json()
 
-
-def create_url(user_names_list, user_fields ):
-    # Specify the usernames that you want to lookup below
-    # You can enter up to 100 comma-separated values.
-    user_names = ','.join(user_names_list) if len(user_names_list)>1 else user_names_list[0]
+    def get_follower_counts(self):
+        url = self.create_url()
+        json_response = self.connect_to_endpoint(url)
+        follower_counts = [user_data['public_metrics']['followers_count'] for user_data in json_response['data']]
+        return follower_counts
     
-    usernames = f"usernames={user_names}"
-    url = "https://api.twitter.com/2/users/by?{}&{}".format(usernames, user_fields)
-    print("url formed", url)
-    return url
-
-
-
-def bearer_oauth(r):
-    r.headers["Authorization"] = f"Bearer {bearer_token}"
-    r.headers["User-Agent"] = "v2UserLookupPython"
-    
-    print(r)
-    return r
-
-def connect_to_endpoint(url):
-    response = requests.request("GET", url, auth=bearer_oauth,)
-    print(f"response.status_code: {response.status_code}")
-    if response.status_code != 200:
-        raise Exception(
-            "Request returned an error: {} {}".format(
-                response.status_code, response.text
-            )
-        )
-    return response.json()
-
-url = create_url(["VisualStockRSRC"], "user.fields=public_metrics")
-json.response = connect_to_endpoint(url)
